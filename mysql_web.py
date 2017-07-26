9c571b652169c425e407738c76f8c34aa20e8d74
Tim <ycg166911@163.com>
2017-07-26 18:33:38 +0800

    提交文件

--- mysql_web.py
+++ mysql_web.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-#yum install openssl-devel python-devel libffi-devel -y
-#pip install flask flask-login gevent threadpool pymysql DBUtils paramiko
+# yum install openssl-devel python-devel libffi-devel -y
+# pip install flask flask-login gevent threadpool pymysql DBUtils paramiko
 
 import json
 import os
@@ -11,23 +11,23 @@ import base64
 import sys
 
 from gevent import pywsgi
-from flask import Flask, render_template, request, app, redirect, url_for
+from flask import Flask, render_template, request, app, redirect, url_for, session
 from flask_login import login_user, login_required, logout_user, LoginManager, current_user
 
-import settings
-import backup
+import settings, backup
 from monitor import user_login, base_class, new_slow_log, report
 from monitor.alarm_server import alarm_thread, execute_sql, general_log, thread, user
-from monitor import cache, server, slow_log, mysql_status, tablespace, general_log, execute_sql, user, thread, chart
+from monitor import cache, server, slow_log, mysql_status, tablespace, chart
 
 
-#region load data on run
+# region load data on run
 
 reload(sys)
 sys.setdefaultencoding("UTF-8")
 
 app = Flask("mysql_web")
 app.secret_key = os.urandom(24)
+app.config["SECRET_KEY"] = os.urandom(24)
 login_manager = LoginManager()
 login_manager.session_protection = "strong"
 login_manager.login_view = "login_home"
@@ -38,19 +38,17 @@ mysql_cache.load_all_host_infos()
 monitor_server = server.MonitorServer()
 monitor_server.load()
 monitor_server.start()
-#slow_log.load_slow_log_table_config()
-#monitor_server.invoke_check_tablespace_method()
-#alarm_thread.AlarmLog().start()
-#new_alarm.AlarmServer().start()
+monitor_server.invoke_check_tablespace_method()
 
-#endregion
+# endregion
 
-#region mysql api
+# region mysql api
 
 @app.route("/mysql", methods=['GET', 'POST'])
 @login_required
 def get_mysql_data():
     return gzip_compress(render_template("mysqls.html", mysql_infos=mysql_cache.get_all_host_infos(keys=json.loads(request.values["keys"]))))
+
 
 @app.route("/mysql/<int:id>")
 @login_required
@@ -61,73 +59,83 @@ def get_mysql_data_by_id(id):
                             data_innodb=convert_object_to_list(mysql_cache.get_innodb_infos(id)),
                             data_engine_innodb=mysql_cache.get_engine_innodb_status_infos(id))
 
-#endregion
 
-#region mysql status api
+# endregion
+
+# region mysql status api
 
 @app.route("/status", methods=['GET', 'POST'])
 @login_required
 def get_status_data():
-    #print(current_user.id, current_user.username)
+    # print(current_user.id, current_user.username)
     return gzip_compress(get_monitor_data(data_status=mysql_cache.get_all_status_infos(keys=json.loads(request.values["keys"]))))
+
 
 @app.route("/status/<int:id>")
 @login_required
 def get_status_data_by_id(id):
     return get_monitor_data(data_status=convert_object_to_list(mysql_cache.get_status_infos(id)))
 
-#endregion
 
-#region innodb api
+# endregion
+
+# region innodb api
 
 @app.route("/innodb", methods=['GET', 'POST'])
 @login_required
 def get_innodb_data():
     return gzip_compress(get_monitor_data(data_innodb=mysql_cache.get_all_innodb_infos(keys=json.loads(request.values["keys"]))))
 
+
 @app.route("/innodb/<int:id>")
 @login_required
 def get_innodb_data_by_id(id):
     return get_monitor_data(data_innodb=convert_object_to_list(mysql_cache.get_innodb_infos(id)), data_engine_innodb=mysql_cache.get_engine_innodb_status_infos(id))
 
-#endregion
 
-#region replication api
+# endregion
+
+# region replication api
 
 @app.route("/replication", methods=['GET', 'POST'])
 @login_required
 def get_replication_data():
     return gzip_compress(get_monitor_data(data_repl=mysql_cache.get_all_repl_infos(keys=json.loads(request.values["keys"]))))
 
+
 @app.route("/replication/<int:id>")
 @login_required
 def get_replication_data_by_id(id):
     return get_monitor_data(slave_status=mysql_status.get_show_slave_status(id))
 
-#endregion
 
-#region tablespace api
+# endregion
+
+# region tablespace api
 
 @app.route("/tablespace")
 @login_required
 def get_tablespace():
     return render_template("tablespace.html", host_infos=mysql_cache.get_all_host_infos())
 
+
 @app.route("/tablespace/check/invoke")
 def execute_check_tablespace():
     monitor_server.invoke_check_tablespace_method()
     return "invoke ok, please wait."
 
+
 @app.route("/tablespace/sort/", methods=["POST"])
 @login_required
 def sort_tablespace():
     json_obj = get_object_from_json(request.form)
-    if(json_obj.host_id <= 0):
+    if (json_obj.host_id <= 0):
         return get_table_html(tablespace_list=tablespace.sort_tablespace(json_obj.sort_type_id))
     else:
         return get_table_html(page_number=json_obj.page_number,
                               page_list=get_page_number_list(json_obj.page_number),
                               table_list=tablespace.sort_tablespace_by_host_id(json_obj.host_id, json_obj.sort_type_id, json_obj.page_number, json_obj.table_name))
+
 
 @app.route("/tablespace/report")
 @login_required
@@ -135,50 +143,58 @@ def send_tablespace_report_mail():
     report.send_report_everyday()
     return "send ok"
 
+
 @app.route("/tablespace/table/detail", methods=["POST"])
 @login_required
 def get_table_detail():
     return get_table_html(table_detail=tablespace.get_table_info(int(request.form["host_id"]), request.form["table_schema"], request.form["table_name"]))
 
+
 def get_table_html(tablespace_list=None, table_list=None, table_detail=None, page_number=1, page_list=None):
     return render_template("tablespace_dispaly.html", host_tablespace_infos=tablespace_list, tablespace_status=table_list, table_detail_info=table_detail, page_number=page_number, page_list=page_list)
+
 
 @app.route("/tablespace/search", methods=["POST"])
 @login_required
 def search_table():
     return get_table_html(table_list=tablespace.search_table(int(request.form["host_id"]), request.form["table_name"]))
 
-#endregion
 
-#region general log api
+# endregion
+
+# region general log api
 
 @app.route("/general/<int:page_number>")
 @login_required
 def get_general_log_by_page_number(page_number):
-    if(page_number <= 5):
+    if (page_number <= 5):
         page_list = range(1, 10)
     else:
-        page_list = range(page_number-5, page_number + 6)
+        page_list = range(page_number - 5, page_number + 6)
     return render_template("general_log.html", general_logs=general_log.get_general_logs_by_page_index(page_number), page_number=page_number, page_list=page_list)
+
 
 @app.route("/general/<int:page_number>/detail/<checksum>")
 @login_required
 def get_general_log_detail(page_number, checksum):
     return render_template("general_log_detail.html", page_number=page_number, general_log_detail=general_log.get_general_log_detail(checksum))
 
+
 @app.route("/general/review/<int:checksum>")
 @login_required
 def set_general_log_is_review(checksum):
     return general_log.set_general_log_is_review(checksum)
+
 
 @app.route("/general/review/<int:host_id>/<int:checksum>")
 @login_required
 def set_general_log_is_review_by_host_id(host_id, checksum):
     return general_log.set_general_log_is_review_by_host_id(host_id, checksum)
 
-#endregion
 
-#region common methon
+# endregion
+
+# region common methon
 
 def gzip_compress(content):
     zbuf = StringIO.StringIO()
@@ -187,6 +203,7 @@ def gzip_compress(content):
     zfile.close()
     return base64.b64encode(zbuf.getvalue())
 
+
 def gzip_decompress(content):
     compresseddata = base64.b64decode(content)
     compressedstream = StringIO.StringIO(compresseddata)
@@ -194,50 +211,58 @@ def gzip_decompress(content):
     data = gzipper.read()
     return data
 
+
 def convert_object_to_list(obj):
     list_tmp = None
-    if(obj != None):
+    if (obj != None):
         list_tmp = []
         list_tmp.append(obj)
     return list_tmp
 
+
 def get_monitor_data(data_status=None, data_innodb=None, data_repl=None, data_engine_innodb=None, data_host=None, slave_status=None, tablespace_status=None):
     return render_template("monitor.html", data_engine_innodb=data_engine_innodb, data_status=data_status, data_innodb=data_innodb, data_repl=data_repl, data_host=data_host, slave_status=slave_status, tablespace_status=tablespace_status)
 
-#endregion
 
-#region slow log api
+# endregion
+
+# region slow log api
 
 @app.route("/slowlog")
 @login_required
 def slow_log_home():
     return render_template("new_slow_log_home.html", host_infos=mysql_cache.get_all_host_infos(), user_infos=mysql_cache.get_mysql_web_user_infos())
 
+
 @app.route("/slowlog/<int:query_type_id>")
 @login_required
 def get_slow_logs(query_type_id):
     return render_template("slow_log_display.html", slow_log_infos=slow_log.get_all_slow_infos(0, query_type_id))
+
 
 @app.route("/slowlog/config/load/")
 @login_required
 def load_log_table_config():
     slow_log.load_slow_log_table_config()
 
+
 @app.route("/slowlog/detail/<int:host_id>/<int:checksum>")
 @login_required
 def get_detail(host_id, checksum):
     return slow_log.get_slow_log_detail_by_host_id(host_id, checksum)
+
 
 @app.route("/slowlog/detail/new/<int:checksum>")
 @login_required
 def get_slow_log_detail(checksum):
     return render_template("slow_log_detail.html", slow_low_info=slow_log.get_slow_log_detail(checksum))
 
+
 @app.route("/newslowlog/", methods=['POST'])
 @login_required
 def new_get_slow_logs():
     page_number = 1
-    if(request.form.to_dict().has_key("page_number")):
+    if (request.form.to_dict().has_key("page_number")):
         page_number = int(request.form["page_number"])
     return render_template("slow_log_display.html",
                            slow_logs=new_slow_log.get_slow_logs(request.form["host_ids"],
@@ -250,18 +275,22 @@ def new_get_slow_logs():
                            page_number=page_number,
                            page_list=get_page_number_list(page_number))
 
+
 @app.route("/newslowlog/detail/<int:checksum>/<int:host_id>")
 @login_required
 def new_get_slow_log_detail(checksum, host_id):
     return render_template("slow_log_detail.html", slow_low_info=new_slow_log.get_slow_log_detail(checksum, host_id))
 
+
 @app.route("/newslowlog/explain/<int:checksum>/<int:host_id>")
 def get_explain_infos(checksum, host_id):
     return render_template("slow_log_detail.html", slow_low_info=new_slow_log.get_slow_log_detail(checksum, host_id))
 
+
 @app.route("/newslowlog/review/detail/<int:checksum>")
 def get_review_detail(checksum):
     return new_slow_log.get_review_detail_by_checksum(checksum)
+
 
 @app.route("/newslowlog/review/update", methods=['POST'])
 def update_review_detail():
@@ -269,59 +298,68 @@ def update_review_detail():
     obj.user_id = current_user.id
     return new_slow_log.update_review_detail(obj)
 
+
 def get_page_number_list(page_number):
-    if(page_number <= 5):
+    if (page_number <= 5):
         page_list = range(1, 10)
     else:
         page_list = range(page_number - 5, page_number + 6)
     return page_list
 
-#endregion
 
-#region execute sql api
+# endregion
+
+# region execute sql api
 
 @app.route("/sql")
 @login_required
 def execute_sql_home():
     return render_template("execute_sql.html", host_infos=mysql_cache.get_all_host_infos())
 
+
 @app.route("/autoreview", methods=['GET', 'POST'])
 @login_required
 def execute_sql_for_commit():
     return execute_sql.execute_sql_test(request.form["cluster_name"], request.form["sql_content"], request.form["workflow_name"], request.form["is_backup"])
 
-#endregion
 
-#region other api
+# endregion
+
+# region other api
 
 @app.route("/os", methods=['GET', 'POST'])
 @login_required
 def get_os_infos():
     return gzip_compress(get_monitor_data(data_host=mysql_cache.get_all_linux_infos(keys=json.loads(request.values["keys"]))))
 
+
 @app.route("/home", methods=['GET', 'POST'])
 @login_required
 def home():
     return render_template("home.html", host_infos=mysql_cache.get_all_host_infos(), username=current_user.username, interval=settings.UPDATE_INTERVAL * 1000)
+
 
 @app.route("/home/binlog")
 @login_required
 def get_test():
     return render_template("binlog.html")
 
+
 @app.route("/home/load")
 def load_all_host_infos():
     mysql_cache.load_all_host_infos()
     return "load ok"
 
-#endregion
 
-#region user web api
+# endregion
+
+# region user web api
 
 @app.route("/user")
 @login_required
 def user_home():
     return render_template("user.html", host_infos=mysql_cache.get_all_host_infos())
+
 
 @app.route("/user/query", methods=['GET', 'POST'])
 @login_required
@@ -330,47 +368,55 @@ def select_user():
     host_id = int(request.form["server_id"])
     return render_template("user_display.html", host_id=host_id, user_infos=user.MySQLUser(host_id).query_user(request.form["user_name"], request.form["ip"]))
 
+
 @app.route("/user/db")
 @login_required
 def get_all_database_name():
     return user.MySQLUser(1).get_all_database_name()
+
 
 @app.route("/user/<name>/<ip>")
 @login_required
 def get_detail_priv_by_user(name, ip):
     return user.MySQLUser(1).get_privs_by_user(name, ip)
 
+
 @app.route("/user/detail/<int:host_id>/<name>/<ip>")
 @login_required
 def get_user_detail(host_id, name, ip):
     return user.MySQLUser(host_id).get_privs_by_user(name, ip)
 
+
 @app.route("/user/add")
 def add_user():
     pass
+
 
 @app.route("/user/drop/<int:host_id>/<name>/<ip>")
 @login_required
 def drop_user(host_id, name, ip):
     return user.MySQLUser(host_id).drop_user(name, ip)
 
-#endregion
 
-#region thread api
+# endregion
+
+# region thread api
 
 @app.route("/thread")
 @login_required
 def thread_home():
     return render_template("thread.html", host_infos=mysql_cache.get_all_host_infos())
 
+
 @app.route("/thread/<int:host_id>/<int:query_type>")
 @login_required
 def get_thread_infos(host_id, query_type):
     return render_template("thread_display.html", thread_infos=thread.get_all_thread(host_id, query_type))
 
-#endregion
 
-#region login api
+# endregion
+
+# region login api
 
 @app.route("/login/verfiy", methods=['GET', 'POST'])
 def login_verfiy():
@@ -378,9 +424,10 @@ def login_verfiy():
     result.error = ""
     result.success = ""
     user_tmp = user_login.User(request.form["userName"])
-    if(user_tmp.verify_password(request.form["passWord"], result) == True):
+    if (user_tmp.verify_password(request.form["passWord"], result) == True):
         login_user(user_tmp)
     return json.dumps(result, default=lambda o: o.__dict__)
+
 
 @app.route("/logout")
 @login_required
@@ -388,51 +435,60 @@ def logout():
     logout_user()
     return redirect(url_for("login"))
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return user_login.User(None).get(user_id)
+
 
 @app.route("/login")
 def login_home():
     return "<p hidden>login_error</p>" + render_template("login.html")
 
-#endregion
 
-#region chart api
+# endregion
+
+# region chart api
 
 @app.route("/chart")
 @login_required
 def chart_home():
     return render_template("chart.html", host_infos=mysql_cache.get_all_host_infos(), interval=settings.UPDATE_INTERVAL * 1000)
 
+
 @app.route("/chart/<int:host_id>")
 @login_required
 def get_chart_data_by_host_id(host_id):
     return chart.get_chart_data_by_host_id(host_id)
+
 
 @app.route("/chart_new")
 @login_required
 def chart_home_new():
     return render_template("chart_new.html", host_infos=mysql_cache.get_all_host_infos(), chart_options=chart.chart_options)
 
+
 @app.route("/chart_new/get_data/", methods=["POST"])
 @login_required
 def get_chart_data_by_attribute_names():
     return chart.get_chart_data(get_object_from_json(request.form))
+
 
 @app.route("/chart_new/option/<int:key>", methods=["GET", "POST"])
 @login_required
 def get_chart_options(key):
     return chart.get_chart_option(key)
 
-#endregion
 
-#region config
+# endregion
+
+# region config
 
 @app.route("/config")
 @login_required
 def get_config_html():
     return render_template("config.html", config_info=get_config_options_value())
+
 
 @app.route("/config/update", methods=["POST"])
 @login_required
@@ -450,6 +506,7 @@ def update_config_options():
         settings.EMAIL_SEND_USERS = obj.email_send_users
     return "save success."
 
+
 def get_config_options_value():
     info = base_class.BaseClass(None)
     info.host = settings.EMAIL_HOST
@@ -462,50 +519,56 @@ def get_config_options_value():
     info.innodb_engine_refresh = settings.INNODB_UPDATE_INTERVAL
     return info
 
+
 def get_object_from_json(json_value):
     obj = base_class.BaseClass(None)
     for key, value in dict(json_value).items():
-        if(value[0].isdigit()):
+        if (value[0].isdigit()):
             setattr(obj, key, int(value[0]))
         else:
             setattr(obj, key, value[0])
     return obj
 
-#endregion
 
-#region backup
+# endregion
+
+# region backup
 
 @app.route("/backup")
 @login_required
 def get_backup_html():
-    return render_template("backup.html", backup_infos=backup.backup_infos, host_infos=mysql_cache.get_all_host_infos())
+    return render_template("backup.html", host_infos=mysql_cache.get_all_host_infos())
+
 
 @app.route("/backup/add", methods=["POST"])
 @login_required
 def add_backup_task():
     return backup.add_backup(get_object_from_json(request.form))
 
-#endregion
 
-#region mysql exception log
+# endregion
+
+# region mysql exception log
 
 @app.route("/mysql_log/all", methods=["POST"])
 @login_required
 def get_mysql_exception_logs():
     return render_template("mysql_exception_log_display.html", mysql_logs=alarm_thread.get_execption_logs(get_object_from_json(request.form)))
 
+
 @app.route("/mysql_log/")
 @login_required
 def get_mysql_exception_log_html():
     return render_template("mysql_exception_log.html", host_infos=mysql_cache.get_all_host_infos())
 
-#endregion
+
+# endregion
 
 if __name__ == '__main__':
-    if(settings.LINUX_OS):
+    if (settings.LINUX_OS):
         print("linux start ok.")
         server = pywsgi.WSGIServer(("0.0.0.0", 5000), app)
         server.serve_forever()
-    if(settings.WINDOWS_OS):
+    if (settings.WINDOWS_OS):
         print("windows start ok.")
         app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
