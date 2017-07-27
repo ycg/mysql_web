480540dd3b9d3eb3e07950b42cee9fa6a8a5ac75
Tim <ycg166911@163.com>
2017-07-27 18:40:37 +0800

    进一步优化表空间检查页面布局

--- monitor/tablespace.py
+++ monitor/tablespace.py
@@ -27,23 +27,26 @@ class TableInfo():
 
 def get_table_infos(host_info):
     table_infos = {}
-    sql = "select table_schema, table_name, DATA_LENGTH, INDEX_LENGTH, TABLE_ROWS, AUTO_INCREMENT, create_time, engine, update_time " \
-          "from information_schema.tables " \
-          "where table_schema != 'mysql' and table_schema != 'information_schema' and table_schema != 'performance_schema' and table_schema != 'sys'";
-    for row in db_util.DBUtil().fetchall(host_info, sql):
-        table_info = TableInfo()
-        table_info.schema = row["table_schema"]
-        table_info.t_name = row["table_name"]
-        table_info.rows = row["TABLE_ROWS"]
-        table_info.data_size = row["DATA_LENGTH"] if row["DATA_LENGTH"] else 0
-        table_info.index_size = row["INDEX_LENGTH"] if row["INDEX_LENGTH"] else 0
-        table_info.auto_increment = row["AUTO_INCREMENT"] if row["AUTO_INCREMENT"] else 0
-        table_info.total_size = long(table_info.data_size) + long(table_info.index_size)
-        table_info.create_time = row["create_time"]
-        table_info.update_time = row["update_time"] if row["update_time"] else ''
-        #table_info.has_primary_key = check_table_has_primary_key(table_info.schema, table_info.t_name)
-        table_name = row["table_schema"] + "." + row["table_name"]
-        table_infos[table_name] = table_info
+    db_names = db_util.DBUtil().fetchall(host_info, "show databases;")
+    for db_name in db_names:
+        sql = "select table_schema, table_name, DATA_LENGTH, INDEX_LENGTH, TABLE_ROWS, AUTO_INCREMENT, create_time, engine, update_time " \
+              "from information_schema.tables " \
+              "where  table_schema = '{0}' and " \
+              "table_schema != 'mysql' and table_schema != 'information_schema' and table_schema != 'performance_schema' and table_schema != 'sys'".format(db_name["Database"])
+        for row in db_util.DBUtil().fetchall(host_info, sql):
+            table_info = TableInfo()
+            table_info.schema = row["table_schema"]
+            table_info.t_name = row["table_name"]
+            table_info.rows = row["TABLE_ROWS"]
+            table_info.data_size = row["DATA_LENGTH"] if row["DATA_LENGTH"] else 0
+            table_info.index_size = row["INDEX_LENGTH"] if row["INDEX_LENGTH"] else 0
+            table_info.auto_increment = row["AUTO_INCREMENT"] if row["AUTO_INCREMENT"] else 0
+            table_info.total_size = long(table_info.data_size) + long(table_info.index_size)
+            table_info.create_time = row["create_time"]
+            table_info.update_time = row["update_time"] if row["update_time"] else ''
+            table_info.engine = row["engine"]
+            table_name = row["table_schema"] + "." + row["table_name"]
+            table_infos[table_name] = table_info
     return table_infos
 
 def check_table_has_primary_key(table_schema, table_name):
@@ -193,6 +196,8 @@ def sort_tablespace_by_host_id(host_id, sort_type, page_number, table_name):
         result_table_list = sorted(infos, cmp=lambda x, y: cmp(x.total_size_o, y.total_size_o), reverse=True)
     elif(sort_type == 5):
         result_table_list = sorted(infos, cmp=lambda x, y: cmp(x.file_size_o, y.file_size_o), reverse=True)
+    elif(sort_type == 7):
+        result_table_list = sorted(infos, cmp=lambda x, y: cmp(x.auto_increment, y.auto_increment), reverse=True)
     else:
         result_table_list = sorted(infos, cmp=lambda x, y: cmp(x.free_size, y.free_size), reverse=True)
 
