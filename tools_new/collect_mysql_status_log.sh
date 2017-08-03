2bcace4c504e4b2135aeda509b3811786849e33e
Tim <ycg166911@163.com>
2017-08-03 16:44:10 +0800

    完成收集mysql运行状态的脚本编写
    还是比较方便的

---
+++ tools_new/collect_mysql_status_log.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# 用于在固定时间段收集mysql运行状态信息的脚本
+# 参数详解
+# host_list 需要收集的mysql机器host列表，已空格分割
+# start_timestamp 收集日志的开始时间
+# end_timestamp 收集日志的结束时间
+# user 数据库用户名
+# password 数据库密码
+# 注意点：尽量数据库用同一个账号
+# 日志保存在/tmp目录下
+
+
+user=""
+password=""
+host_list=("192.168.1.101" "192.168.1.102")
+
+start_timestamp=`date +%s -d "2017-08-04 00:49:00"`
+end_timestamp=`date +%s -d "2017-08-04 00:53:00"`
+echo ${start_timestamp}, ${end_timestamp}
+
+innodb_engine_status="show engine innodb status\G"
+show_processlist="SELECT * FROM information_schema.processlist where COMMAND != 'Sleep'\G"
+
+function collect_status()
+{
+    for host in ${host_list[*]}
+    do
+        processlist_file="/tmp/${host}_processlist.txt"
+        innodb_status_file="/tmp/${host}_innodb_status.txt"
+        echo "-----------------------------------------`date "+%Y-%m-%d %H:%M:%S"`-------------------------------------------" >> ${processlist_file}
+        mysql -h${host} -u${user} -p${password} -e"$show_processlist" >> ${processlist_file}
+        echo "" >> ${processlist_file}
+
+        echo "-----------------------------------------`date "+%Y-%m-%d %H:%M:%S"`-------------------------------------------" >> ${innodb_status_file}
+        mysql -h${host} -u${user} -p${password} -e"$innodb_engine_status" >> ${innodb_status_file}
+        echo "" >> ${innodb_status_file}
+    done
+}
+
+now_timestamp=`date +%s`
+while [ ${now_timestamp} -le ${end_timestamp} ]
+do
+    if [ ${now_timestamp} -ge ${start_timestamp} ] && [ ${now_timestamp} -le ${end_timestamp} ]; then
+        collect_status
+        echo "collect ok"
+    else
+        echo "no collect"
+    fi
+    sleep 1
+    now_timestamp=`date +%s`
+done
+
+echo "collect mysql log stop"
