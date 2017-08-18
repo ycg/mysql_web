d1f5ef6909071e7358aad7d5d3f618333eea14bd
Tim <ycg166911@163.com>
2017-08-18 14:51:57 +0800

    tijiao

--- tools_new/mysql_auto_install.py
+++ tools_new/mysql_auto_install.py
@@ -2,40 +2,41 @@
 
 import os, paramiko, argparse, sys, time
 
-#首先安装依赖包 - pip install paramiko
+# 首先安装依赖包 - pip install paramiko
 
-#1.生成配置文件
-    #获取buffer pool的大小
-    #创建各种目录 - mkdir
-#2.从服务器获取安装包
-    #scp /opt/mysql.5.6.tar.gz root@192.168.11.129:/opt/
-    #mkdir /usr/local/mysql
-    #tar -zxvf /opt/mysql.5.6.tar.gz --strip-components=1 -C /usr/local/mysql
-#3.创建用户
-    #groupadd mysql
-    #useradd mysql -g mysql
-    #chmod -R mysql:mysql /mysql_data/
-    #chmod -R mysql:mysql /mysql_binlog/
-#3.自动初始化数据
-    #5.6和5.7的方式不一样
-    #5.6
-        #yum install -y perl-Module-Install.noarch
-        #/usr/local/mysql/script/mysql_install_db --defaults-file=/etc/my.cnf > /tmp/mysql_install.log
-    #5.7
-        #/usr/local/mysql/bin/mysqld --defaults-file=/etc/my.cnf --initialize-insecure > /tmp/mysql_install.log
-#4.自动启动
-    #/usr/local/mysql/bin/mysqld --defaults-file=/etc/my.cnf &
+# 1.生成配置文件
+#   获取buffer pool的大小
+#   创建各种目录 - mkdir
+# 2.从服务器获取安装包
+#   scp /opt/mysql.5.6.tar.gz root@192.168.11.129:/opt/
+#   mkdir /usr/local/mysql
+#   tar -zxvf /opt/mysql.5.6.tar.gz --strip-components=1 -C /usr/local/mysql
+# 3.创建用户
+#   groupadd mysql
+#   useradd mysql -g mysql
+#   chmod -R mysql:mysql /mysql_data/
+#   chmod -R mysql:mysql /mysql_binlog/
+# 3.自动初始化数据
+#   5.6和5.7的方式不一样
+# 5.6
+#   yum install -y perl-Module-Install.noarch
+#   /usr/local/mysql/script/mysql_install_db --defaults-file=/etc/my.cnf > /tmp/mysql_install.log
+# 5.7
+#   /usr/local/mysql/bin/mysqld --defaults-file=/etc/my.cnf --initialize-insecure > /tmp/mysql_install.log
+# 4.自动启动
+#   /usr/local/mysql/bin/mysqld --defaults-file=/etc/my.cnf &
 
-#5.脚本示例
-#python mysql_auto_install.py --host=192.168.11.129 --version=5.6 --package=/opt/mysql-5.6.tar.gz
-#--host：需要安装的主机ip
-#--port：指定mysql端口，多实例安装需要用到
-#--version：安装包的版本
-#--package：安装包路径
-#--data-dir：指定数据存储目录
-#--binlog-dir：指定binlog存储目录
-#--gtid：是否使用gtid模式
-#--semi-sync：是否使用半同步模式来保证数据的一致性
+# 5.脚本示例
+# python mysql_auto_install.py --host=192.168.11.129 --version=5.6 --package=/opt/mysql-5.6.tar.gz
+# --host：需要安装的主机ip
+# --port：指定mysql端口，多实例安装需要用到
+# --version：安装包的版本
+# --package：安装包路径
+# --based-dir：指定mysql安装路径
+# --data-dir：指定数据存储目录
+# --binlog-dir：指定binlog存储目录
+# --gtid：是否使用gtid模式
+# --semi-sync：是否使用半同步模式来保证数据的一致性
 
 error = "error"
 output = "output"
@@ -44,6 +45,7 @@ mysql_5_7 = "5.7"
 data_dir = "/mysql/data"
 binlog_dir = "/mysql/binlog"
 base_dir = "/usr/local/mysql"
+
 
 def check_arguments():
     global data_dir, base_dir, binlog_dir
@@ -62,7 +64,7 @@ def check_arguments():
     if not args.host or not args.version:
         print("[error]:Please input remote host ip or hostname.")
         sys.exit(1)
-    if(args.version != "5.6" and args.version != "5.7"):
+    if (args.version != "5.6" and args.version != "5.7"):
         print("[error]:Please input mysql package version number [--version=5.6 | --version=5.7].")
         sys.exit(1)
     if not args.package:
@@ -73,13 +75,14 @@ def check_arguments():
     args.package_name = os.path.basename(args.package)
     return args
 
+
 def mysql_install(args):
-    #创建ssh对象
+    # 创建ssh对象
     host_client = paramiko.SSHClient()
     host_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     host_client.connect(args.host, port=22, username="root")
 
-    #创建目录
+    # 创建目录
     print("\n--------------------------1.rm dir and create mysql data dir-------------------")
     kill_mysql_process(host_client)
     execute_remote_shell(host_client, "rm -rf {0}".format(base_dir))
@@ -89,7 +92,7 @@ def mysql_install(args):
     execute_remote_shell(host_client, "mkdir -p {0}".format(data_dir))
     execute_remote_shell(host_client, "mkdir -p {0}".format(binlog_dir))
 
-    #生成配置文件并同步到远程机器
+    # 生成配置文件并同步到远程机器
     print("\n--------------------------2.geneate mysql config-------------------------------")
     server_id = get_server_id(host_client, args)
     buffer_pool_size, buffer_pool_instance = get_mysql_buffer_pool_size(host_client)
@@ -98,39 +101,40 @@ def mysql_install(args):
     config_value = check_use_repl_semi_sync(args, config_value)
     write_mysql_conf_to_file(args, config_value)
 
-    #拷贝二进制包和解压
+    # 拷贝二进制包和解压
     print("\n--------------------------3.copy mysql install package and unzip---------------")
     os.system("scp {0} root@{1}:/opt/".format(args.package, args.host))
     execute_remote_shell(host_client, "tar -zxvf /opt/{0} --strip-components=1 -C {1}".format(args.package_name, base_dir))
 
-    #创建用户和赋值权限
+    # 创建用户和赋值权限
     execute_remote_shell(host_client, "groupadd mysql")
     execute_remote_shell(host_client, "useradd mysql -g mysql")
     execute_remote_shell(host_client, "chown -R mysql:mysql {0}".format(data_dir))
     execute_remote_shell(host_client, "chown -R mysql:mysql {0}".format(binlog_dir))
 
-    #初始化数据和启动mysql
+    # 初始化数据和启动mysql
     print("\n--------------------------4.init mysql data------------------------------------")
-    if(args.version == mysql_5_6):
+    if (args.version == mysql_5_6):
         execute_remote_shell(host_client, "yum install -y perl-Module-Install.noarch")
         execute_remote_shell(host_client, "{0}/scripts/mysql_install_db --defaults-file=/etc/my.cnf --basedir={1}".format(base_dir, base_dir))
     else:
         execute_remote_shell(host_client, "{0}/bin/mysqld --defaults-file=/etc/my.cnf --initialize-insecure".format(base_dir))
 
-    #要暂停一会，因为前面mysql初始化的时候进程还没有释放，就立刻启动会出现错误
-    if(check_mysqld_pid_is_exists(host_client)):
+    # 要暂停一会，因为前面mysql初始化的时候进程还没有释放，就立刻启动会出现错误
+    if (check_mysqld_pid_is_exists(host_client)):
         execute_remote_shell(host_client, "{0}/bin/mysqld --defaults-file=/etc/my.cnf".format(base_dir))
-    #通过mysqld_safe进行启动，不建议这样的方式
-    #execute_remote_shell(host_client, "cp {0}/support-files/mysql.server /etc/rc.d/init.d/mysqld".format(base_dir))
-    #execute_remote_shell(host_client, "service mysqld start")
+    # 通过mysqld_safe进行启动，不建议这样的方式
+    # execute_remote_shell(host_client, "cp {0}/support-files/mysql.server /etc/rc.d/init.d/mysqld".format(base_dir))
+    # execute_remote_shell(host_client, "service mysqld start")
 
     host_client.close()
     print("\n--------------------------5.mysql install complete ok.-------------------------")
 
+
 def kill_mysql_process(host_client):
-    #改进kill掉mysql的逻辑
+    # 改进kill掉mysql的逻辑
     result = execute_remote_shell(host_client, "cat {0}/mysql.pid".format(data_dir))
-    if(len(result[error]) <= 0):
+    if (len(result[error]) <= 0):
         mysql_pid = result[output][0].replace("\n", "")
         execute_remote_shell(host_client, "kill -6 " + mysql_pid)
     '''
@@ -138,31 +142,35 @@ def kill_mysql_process(host_client):
     for pid in result[output]:
         execute_remote_shell(host_client, "kill -6 {0}".format(pid.replace("\n", "")))'''
 
+
 def get_server_id(host_client, args):
     result = execute_remote_shell(host_client, "ip addr | grep inet | grep -v 127.0.0.1 | grep -v inet6 "
                                                "| awk \'{ print $2}\' | awk -F \"/\" \'{print $1}\' | awk -F \".\" \'{print $4}\'")
     return args.port + result[output][0].replace("\n", "")
 
+
 def check_use_gtid_mode(args, config_value):
-    if(args.gtid == 1):
+    if (args.gtid == 1):
         config_value = config_value + "\n" + gtid_config
     return config_value
 
+
 def check_use_repl_semi_sync(args, config_value):
-    if(args.semi_sync == 1):
+    if (args.semi_sync == 1):
         config_value = config_value + "\n" + rpl_semi_sync_config
-        if(args.version == mysql_5_7):
+        if (args.version == mysql_5_7):
             config_value += "rpl_semi_sync_master_wait_point=AFTER_SYNC"
     return config_value
 
+
 def check_mysqld_pid_is_exists(host_client):
     number = 1
-    while(number <= 10):
+    while (number <= 10):
         result = execute_remote_shell(host_client, "ps -ef | grep mysqld")
-        if(len(result[output]) <= 0):
+        if (len(result[output]) <= 0):
             return True
         else:
-            if(result[output][0].find("defaults-file") < 0):
+            if (result[output][0].find("defaults-file") < 0):
                 return True
             else:
                 print("mysqld init pid is exists.")
@@ -170,23 +178,25 @@ def check_mysqld_pid_is_exists(host_client):
         number += 1
     return True
 
+
 def get_mysql_buffer_pool_size(host_client):
     buffer_pool_instance = 0
     result = execute_remote_shell(host_client, "free -g | head -n2 | tail -n1 | awk \'{print $2}\'")
     total_memory = int(result[output][0].replace("\n", ""))
     buffer_pool_size = str(int(round(total_memory * 0.75))) + "G"
-    if(total_memory == 0):
+    if (total_memory == 0):
         buffer_pool_size = "500M"
         buffer_pool_instance = 1
-    elif(total_memory > 0 and total_memory <= 2):
+    elif (total_memory > 0 and total_memory <= 2):
         buffer_pool_instance = 2
-    elif(total_memory > 2 and total_memory <= 8):
+    elif (total_memory > 2 and total_memory <= 8):
         buffer_pool_instance = 3
-    elif(total_memory > 8 and total_memory <= 16):
+    elif (total_memory > 8 and total_memory <= 16):
         buffer_pool_instance = 4
-    elif(total_memory > 16):
+    elif (total_memory > 16):
         buffer_pool_instance = 8
     return (buffer_pool_size, buffer_pool_instance)
+
 
 def write_mysql_conf_to_file(args, config_value):
     file_path = "/tmp/my.cnf"
@@ -195,6 +205,7 @@ def write_mysql_conf_to_file(args, config_value):
     file.close()
     os.system("scp {0} root@{1}:/etc/".format(file_path, args.host))
 
+
 def execute_remote_shell(host_client, shell):
     result = {}
     try:
@@ -202,13 +213,14 @@ def execute_remote_shell(host_client, shell):
         stdin, stdout, stderr = host_client.exec_command(shell)
         result[error] = stderr.readlines()
         result[output] = stdout.readlines()
-        if(len(result[error]) > 0):
+        if (len(result[error]) > 0):
             print(result[error][0].replace("\n", ""))
     except:
         host_client.close()
     return result
 
-#region mysql config
+
+# region mysql config
 
 mysql_config = ("""
 [mysql]
@@ -346,7 +358,7 @@ slave_parallel_workers = 8
 slave_parallel_type = LOGICAL_CLOCK
 """
 
-#endregion
+# endregion
 
-if(__name__ == "__main__"):
+if (__name__ == "__main__"):
     mysql_install(check_arguments())
