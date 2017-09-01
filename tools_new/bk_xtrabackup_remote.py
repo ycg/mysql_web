a0de3afb1ba1a27884c8a4f8be332f12fbb8212b
Tim <ycg166911@163.com>
2017-09-01 17:19:29 +0800

    tijiao

--- tools_new/bk_xtrabackup_remote.py
+++ tools_new/bk_xtrabackup_remote.py
@@ -40,9 +40,10 @@ import os, argparse, sys, time, datetime, subprocess, commands
 # --mode：增量还是全量+增量
 # --stream：流式备份方式 1-tar 2-xbstream，默认是xbstream，因为增量必须使用xbstream
 # --compress：压缩方式 1-gzip 2-pigz，默认是gzip压缩
+
 # backup.log各个分割字段含义
-# {0}:{1}:{2}:{3}:{4}:{5}:{6}:{7}
-# 备份模式:备份路径:备份日志路径:备份开始时间:备份结束时间:备份日期:备份是否正常:备份目录名称
+# {0}:{1}:{2}:{3}:{4}:{5}:{6}:{7}:{8}:{9}
+# 备份模式:备份路径:备份文件名:备份日志名:备份开始时间:备份结束时间:备份日期:备份是否正常:流式备份方式:压缩方式
 
 
 SUNDAY_INT = 6
@@ -78,12 +79,12 @@ def check_arguments():
         print("[error]:Please input host or user or password or port")
         sys.exit(1)
 
-    if (args.stream != STREAM_TAR or args.stream != STREAM_XBSTREAM):
-        print("[error]:--stream：流式备份方式 1-tar 2-xbstream，默认是xbstream")
+    if (args.stream != STREAM_TAR and args.stream != STREAM_XBSTREAM):
+        print("[error]:--stream：1-tar 2-xbstream，default xbstream")
         sys.exit(1)
 
-    if (args.compress != GZIP_COMPRESS or args.compress != PIGZ_COMPRESS):
-        print("[error]:--compress：压缩方式 1-gzip 2-pigz，默认是gzip压缩")
+    if (args.compress != GZIP_COMPRESS and args.compress != PIGZ_COMPRESS):
+        print("[error]:--compress: 1-gzip 2-pigz，default gzip")
         sys.exit(1)
 
     if (not args.ssh_host):
@@ -91,6 +92,7 @@ def check_arguments():
         args.is_local_backup = True
     else:
         # 远程压缩备份
+        test_ssh_connection(args)
         args.is_local_backup = False
 
     # 检查本地和远程的备份目录是否创建
@@ -108,7 +110,17 @@ def check_arguments():
         args.compress_value = "gzip"
     elif (args.compress == PIGZ_COMPRESS):
         args.compress_value = "pigz"
+    args.backup_log_file_path = os.path.join(args.backup_dir, "backup_log.txt")
     return args
+
+
+# 测试SSH是否正常
+def test_ssh_connection(args):
+    status, output = commands.getstatusoutput("ssh {0}@{1} 'df -h'".format(args.ssh_user, args.ssh_host))
+    if (int(status) > 0):
+        print(output)
+        print("[error]:Please check ssh user or host is correct.")
+        sys.exit(1)
 
 
 # 备份
@@ -139,7 +151,9 @@ def full_backup(args):
         command = full_backup_to_remote(args, file_name)
     execute_xtrabackup_shell_command(args, command, os.path.join(args.backup_dir, log_name))
     stop_backup_time = get_current_time()
-
+    write_backup_log_file(args, args.backup_log_file_path, FULL_BACKUP,
+                          args.backup_dir if (args.is_local_backup) else args.remote_backup_dir,
+                          file_name, log_name, start_backup_time, stop_backup_time, args.stream, args.compress)
 
 # 本地压缩备份
 def full_backup_to_local(args, file_name):
@@ -181,6 +195,9 @@ def increment_backup(args):
         command = increment_backup_to_remote(args, file_name)
     execute_xtrabackup_shell_command(args, command, os.path.join(args.backup_dir, log_name))
     stop_backup_time = get_current_time()
+    write_backup_log_file(args, args.backup_log_file_path, INCREMENT_BACKUP,
+                          args.backup_dir if (args.is_local_backup) else args.remote_backup_dir,
+                          file_name, log_name, start_backup_time, stop_backup_time, STREAM_XBSTREAM, args.compress)
 
 
 # 本地增量备份
@@ -231,6 +248,40 @@ def execute_xtrabackup_shell_command(args, command, log_file_path):
     if (args.is_local_backup == False):
         command = "scp {0} {1}@{2}:{3}".format(log_file_path, args.ssh_user, args.ssh_host, args.remote_backup_dir)
         execute_shell_command(command)
+
+
+# 写入数据到备份日志文件中去
+def write_backup_log_file(args, log_file_path, backup_mode, backup_dir, backup_file_name, xtrabackup_log_name, start_time, stop_time, stream, compress):
+    file = None
+    try:
+        log_value = "{0}:{1}:{2}:{3}:{4}:{5}:{6}:{7}:{8}:{9}\n".format(backup_mode,
+                                                                       backup_dir,
+                                                                       backup_file_name,
+                                                                       xtrabackup_log_name,
+                                                                       start_time,
+                                                                       stop_time,
+                                                                       time.strftime('%Y-%m-%d', time.localtime(time.time())),
+                                                                       check_backup_is_correct(xtrabackup_log_name),
+                                                                       stream,
+                                                                       compress)
+        file = open(log_file_path, "a")
+        file.write(log_value)
+    finally:
+        if (file != None):
+            file.close()
+
+    if (args.is_local_backup == False):
+        command = "scp {0} {1}@{2}:{3}".format(log_file_path, args.ssh_user, args.ssh_host, args.remote_backup_dir)
+        execute_shell_command(command)
+
+
+# 根据xtrabackup的输出日志判断备份是否正常
+def check_backup_is_correct(xtrabackup_log_path):
+    status, output = commands.getstatusoutput("tail -n 1 {0}".format(xtrabackup_log_path))
+    if (int(status) == 0):
+        if (output.find("completed OK") > 0):
+            return 1
+    return 0
 
 
 backup(check_arguments())
