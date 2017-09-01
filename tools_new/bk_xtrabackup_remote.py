bf18ebfd40fa433b34d8a18cbc207a5e275bf5cf
Tim <ycg166911@163.com>
2017-09-01 16:38:39 +0800

    tijiao

--- tools_new/bk_xtrabackup_remote.py
+++ tools_new/bk_xtrabackup_remote.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os, argparse, sys, time, datetime, subprocess, traceback, commands
+import os, argparse, sys, time, datetime, subprocess, commands
 
 # xtrabackup远程流式备份脚本
 # 两种流式备份方式：
@@ -31,18 +31,29 @@ import os, argparse, sys, time, datetime, subprocess, traceback, commands
 
 
 # 本地压缩备份示例
-# python bk_xtrabackup_remote.py --host=192.168.11.101 --user=yangcg --password='yangcaogui' --mode=1 --stream=1 --backup-dir=/opt/backup_compress
+# python bk_xtrabackup_remote.py --host=192.168.11.101 --user=yangcg --password='yangcaogui' --mode=2 --backup-dir=/opt/backup_compress
 
 # 远程压缩备份示例
-# python bk_xtrabackup_remote.py --host=192.168.11.101 --user=yangcg --password='yangcaogui' --mode=2 --stream=1 --ssh-host=master --remote-backup-dir=/opt/backup_compress
+# python bk_xtrabackup_remote.py --host=192.168.11.101 --user=yangcg --password='yangcaogui' --mode=2 --ssh-host=master --remote-backup-dir=/opt/backup_compress
+
+# 参数详解：
+# --mode：增量还是全量+增量
+# --stream：流式备份方式 1-tar 2-xbstream，默认是xbstream，因为增量必须使用xbstream
+# --compress：压缩方式 1-gzip 2-pigz，默认是gzip压缩
+# backup.log各个分割字段含义
+# {0}:{1}:{2}:{3}:{4}:{5}:{6}:{7}
+# 备份模式:备份路径:备份日志路径:备份开始时间:备份结束时间:备份日期:备份是否正常:备份目录名称
+
 
 SUNDAY_INT = 6
 FULL_BACKUP = 1
 INCREMENT_BACKUP = 2
+GZIP_COMPRESS = 1
+PIGZ_COMPRESS = 2
+LOG_FILE_DIR = "/tmp"
 STREAM_TAR = 1
 STREAM_XBSTREAM = 2
 CHECKPOINTS_FILE_NAME = "xtrabackup_checkpoints"
-LOG_FILE_DIR = "/tmp"
 
 
 def check_arguments():
@@ -56,7 +67,8 @@ def check_arguments():
     parser.add_argument("--backup-time", type=str, dest="backup_time", help="help time", default="03:30")
     parser.add_argument("--expire-days", type=int, dest="expire_days", help="expire backup days", default=14)
 
-    parser.add_argument("--stream", type=int, dest="stream", help="--stream", default=STREAM_TAR)
+    parser.add_argument("--stream", type=int, dest="stream", help="--stream", default=STREAM_XBSTREAM)
+    parser.add_argument("--compress", type=int, dest="compress", help="--compress", default=GZIP_COMPRESS)
     parser.add_argument("--ssh-user", type=str, dest="ssh_user", help="backup remote user", default="root")
     parser.add_argument("--ssh-host", type=str, dest="ssh_host", help="backup remote host")
     parser.add_argument("--remote-backup-dir", type=str, dest="remote_backup_dir", help="remote backup dir")
@@ -64,6 +76,14 @@ def check_arguments():
     args = parser.parse_args()
     if (not args.host or not args.user or not args.password or not args.port):
         print("[error]:Please input host or user or password or port")
+        sys.exit(1)
+
+    if (args.stream != STREAM_TAR or args.stream != STREAM_XBSTREAM):
+        print("[error]:--stream：流式备份方式 1-tar 2-xbstream，默认是xbstream")
+        sys.exit(1)
+
+    if (args.compress != GZIP_COMPRESS or args.compress != PIGZ_COMPRESS):
+        print("[error]:--compress：压缩方式 1-gzip 2-pigz，默认是gzip压缩")
         sys.exit(1)
 
     if (not args.ssh_host):
@@ -81,10 +101,13 @@ def check_arguments():
 
     if (args.stream == STREAM_TAR):
         args.stream_value = "tar"
-        args.compress_value = "pigz"
     elif (args.stream == STREAM_XBSTREAM):
         args.stream_value = "xbstream"
+
+    if (args.compress == GZIP_COMPRESS):
         args.compress_value = "gzip"
+    elif (args.compress == PIGZ_COMPRESS):
+        args.compress_value = "pigz"
     return args
 
 
@@ -114,7 +137,7 @@ def full_backup(args):
         command = full_backup_to_local(args, file_name)
     else:
         command = full_backup_to_remote(args, file_name)
-    execute_xtrabackup_shell_command(command, os.path.join(args.backup_dir, log_name))
+    execute_xtrabackup_shell_command(args, command, os.path.join(args.backup_dir, log_name))
     stop_backup_time = get_current_time()
 
 
@@ -156,24 +179,25 @@ def increment_backup(args):
         command = increment_backup_to_local(args, file_name)
     else:
         command = increment_backup_to_remote(args, file_name)
-    execute_xtrabackup_shell_command(command, os.path.join(args.backup_dir, log_name))
+    execute_xtrabackup_shell_command(args, command, os.path.join(args.backup_dir, log_name))
     stop_backup_time = get_current_time()
 
 
 # 本地增量备份
 def increment_backup_to_local(args, file_name):
-    return "innobackupex --host={0} --user={1} --password='{2}' --port={3} --stream=xbstream --slave-info --no-timestamp --incremental --incremental-basedir={4} --extra-lsndir={4} {4} | gzip > {5}" \
+    return "innobackupex --host={0} --user={1} --password='{2}' --port={3} --stream=xbstream --slave-info --no-timestamp --incremental --incremental-basedir={4} --extra-lsndir={4} {4} | {5} > {6}" \
         .format(args.host,
                 args.user,
                 args.password,
                 args.port,
                 args.backup_dir,
+                args.compress_value,
                 os.path.join(args.backup_dir, file_name))
 
 
 # 远程增量备份
 def increment_backup_to_remote(args, file_name):
-    return "innobackupex --host={0} --user={1} --password='{2}' --port={3} --stream=xbstream --slave-info --no-timestamp --incremental --incremental-basedir={4} --extra-lsndir={4} {4} | ssh {5}@{6} \"gzip - > {7}\"" \
+    return "innobackupex --host={0} --user={1} --password='{2}' --port={3} --stream=xbstream --slave-info --no-timestamp --incremental --incremental-basedir={4} --extra-lsndir={4} {4} | ssh {5}@{6} \"{7} - > {8}\"" \
         .format(args.host,
                 args.user,
                 args.password,
@@ -181,6 +205,7 @@ def increment_backup_to_remote(args, file_name):
                 args.backup_dir,
                 args.ssh_user,
                 args.ssh_host,
+                args.compress_value,
                 os.path.join(args.remote_backup_dir, file_name))
 
 
@@ -193,7 +218,7 @@ def execute_shell_command(command):
     result.wait()
 
 
-def execute_xtrabackup_shell_command(command, log_file_path):
+def execute_xtrabackup_shell_command(args, command, log_file_path):
     status, output = commands.getstatusoutput(command)
     file = None
     try:
@@ -202,5 +227,10 @@ def execute_xtrabackup_shell_command(command, log_file_path):
     finally:
         if (file != None):
             file.close()
+
+    if (args.is_local_backup == False):
+        command = "scp {0} {1}@{2}:{3}".format(log_file_path, args.ssh_user, args.ssh_host, args.remote_backup_dir)
+        execute_shell_command(command)
+
 
 backup(check_arguments())
